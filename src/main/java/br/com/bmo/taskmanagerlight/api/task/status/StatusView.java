0c1c7ea205f594ad56918726e574df132b58dc9f
Bruno Moreno <brunomorello7@gmail.com>
2021-04-15 11:38:47 -0300

    DTO view class for Status Moddel

---
+++ src/main/java/br/com/bmo/taskmanagerlight/api/task/status/StatusView.java
@@ -0,0 +1,17 @@
+package br.com.bmo.taskmanagerlight.api.task.status;
+
+public class StatusView {
+
+	private String name;
+	
+	public StatusView(String name) {
+		this.name = name;
+	}
+
+	public StatusView() {}
+	
+	public String getName() {
+		return name;
+	}
+	
+}

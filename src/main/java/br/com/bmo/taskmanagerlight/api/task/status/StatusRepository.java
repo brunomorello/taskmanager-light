85b1a7f92b2b5efcc2d9e50295f8652acbf6a37c
Bruno Moreno <brunomorello7@gmail.com>
2021-04-15 11:36:43 -0300

    Added a list of profiles to be used

--- src/main/java/br/com/bmo/taskmanagerlight/api/task/status/StatusRepository.java
+++ src/main/java/br/com/bmo/taskmanagerlight/api/task/status/StatusRepository.java
@@ -3,8 +3,10 @@ package br.com.bmo.taskmanagerlight.api.task.status;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.Repository;
 
+@Profile(value = {"test", "prod", "dev", "qas"})
 public interface StatusRepository extends Repository<Status, Long> {
 	List<Status> findAll();
 	Optional<Status> findByName(String name);

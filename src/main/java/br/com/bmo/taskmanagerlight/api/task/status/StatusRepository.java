216e80f492d4352b82e854e3f68dbbab631cf37f
Bruno Moreno <brunomorello7@gmail.com>
2021-04-19 16:23:41 -0300

    'Removed unused annotation

--- src/main/java/br/com/bmo/taskmanagerlight/api/task/status/StatusRepository.java
+++ src/main/java/br/com/bmo/taskmanagerlight/api/task/status/StatusRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
 
 @org.springframework.stereotype.Repository
 @Profile(value = {"test", "prod", "dev", "qas"})

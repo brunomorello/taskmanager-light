aab1dc729afb614a259dab089ce37524f35e7fa5
Bruno Moreno <brunomorello7@gmail.com>
2021-04-25 21:16:46 -0300

    simplified constructor and implemented function to add products

--- src/main/java/br/com/bmo/taskmanagerlight/api/shared/domain/Shopping.java
+++ src/main/java/br/com/bmo/taskmanagerlight/api/shared/domain/Shopping.java
@@ -1,16 +1,22 @@
-package br.com.bmo.taskmanagerlight.api.task;
+package br.com.bmo.taskmanagerlight.api.shared.domain;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
 
-import br.com.bmo.taskmanagerlight.api.task.status.Status;
+import br.com.bmo.taskmanagerlight.api.product.Product;
 
 @Entity
 public class Shopping extends Task {
 	
-	public Shopping(String title, String details, Status status, LocalDateTime dueDate) {
-		super(title, details, status, dueDate);
+	private List<Product> products;
+	
+	public Shopping(String title, String details) {
+		super(title, details);
+	}
+	
+	public void addProduct(Product p) {
+		products.add(p);
 	}
 
 }

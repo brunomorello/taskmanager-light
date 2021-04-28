724910a91a45f1afa9b64ff04bf204e2bf168048
Bruno Moreno <brunomorello7@gmail.com>
2021-04-28 15:43:16 -0300

    unit tests

--- src/test/java/br/com/bmo/taskmanagerlight/api/shared/domain/ProductTest.java
+++ src/test/java/br/com/bmo/taskmanagerlight/api/shared/domain/ProductTest.java
@@ -2,6 +2,8 @@ package br.com.bmo.taskmanagerlight.api.shared.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
 
 import br.com.bmo.taskmanagerlight.shared.domain.goods.Product;
@@ -11,11 +13,12 @@ class ProductTest {
 
 	@Test
 	void shouldProductHaveAManufacturerAndPrice() {
-		Product notebook = new Product("Notebook");
+		Product notebook = new Product("Notebook", new BigDecimal("5000"));
 		Manufacturer dell = new Manufacturer("dell");
 		notebook.setManufacturer(dell);
 		assertEquals(notebook.getName(), "Notebook");
 		assertEquals(notebook.getManufacturer(), dell);
+		assertEquals(notebook.getPrice(), new BigDecimal("5000"));
 	}
 
 }

75cb93c5a616be7f8ae401de3cdfa850b1f0ecb7
Bruno Moreno <brunomorello7@gmail.com>
2021-04-29 11:31:05 -0300

    Created a GoodsController interface, validation error response and tests for FoodController

---
+++ src/test/java/br/com/bmo/taskmanagerlight/api/shared/integration/FoodControllerTest.java
@@ -0,0 +1,89 @@
+package br.com.bmo.taskmanagerlight.api.shared.integration;
+
+import static org.junit.Assert.fail;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.bmo.taskmanagerlight.api.goods.food.FoodForm;
+import br.com.bmo.taskmanagerlight.shared.domain.goods.Food;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import br.com.bmo.taskmanagerlight.shared.util.TaskmanagerTestUtils;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestEntityManager
+@Transactional
+@ActiveProfiles("test")
+public class FoodControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private TestEntityManager em;
+	
+	private Food FOOD;
+	
+	private static String BASE_URI = "/api/food/";
+	
+	@BeforeEach
+	void setup() {
+		FOOD = new Food("Beans", new BigDecimal("6.99"));
+		FOOD.setExpirationDate(LocalDate.parse("2021-05-15", DateTimeFormatter.ISO_DATE));
+		em.persist(FOOD);
+	}
+	
+	@Test
+	void shouldReturn200ToFindFoodById() throws Exception {
+		mockMvc.perform(get(BASE_URI + FOOD.getId()))
+			.andDo(log())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name", equalTo("Beans")));
+	}
+	
+	@Test
+	void shouldReturn404WhenFoodIsNotFoundById() throws Exception {
+		mockMvc.perform(get(BASE_URI + "5000012"))
+			.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	void shouldReturn201WhenFoodIsCreated() throws Exception {
+		
+		String payload = TaskmanagerTestUtils.toJsonStr(new FoodForm("Pasta", "2.50", "2020-06-01"));
+		
+		mockMvc.perform(post(BASE_URI).content(payload).contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated());
+	}
+	
+	@Test
+	void shouldReturn200WhenFoodIsUpdated() throws Exception  {
+		mockMvc.perform(put(BASE_URI + FOOD.getId()))
+			.andDo(log())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name", equalTo("Rice")));
+	}
+}

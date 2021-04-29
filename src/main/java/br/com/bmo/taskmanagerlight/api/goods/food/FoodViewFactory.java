2c6d2ff839212b9ce473e220d62207f29e7e3ef4
Bruno Moreno <brunomorello7@gmail.com>
2021-04-29 11:26:25 -0300

    fixed return type

--- src/main/java/br/com/bmo/taskmanagerlight/api/goods/food/FoodViewFactory.java
+++ src/main/java/br/com/bmo/taskmanagerlight/api/goods/food/FoodViewFactory.java
@@ -1,14 +1,13 @@
 package br.com.bmo.taskmanagerlight.api.goods.food;
 
-import br.com.bmo.taskmanagerlight.api.goods.GoodsView;
 import br.com.bmo.taskmanagerlight.api.goods.GoodsViewFactory;
 import br.com.bmo.taskmanagerlight.shared.domain.goods.Food;
 import br.com.bmo.taskmanagerlight.shared.domain.goods.Goods;
 
-public class FoodViewFactory implements GoodsViewFactory{
+public class FoodViewFactory implements GoodsViewFactory {
 
 	@Override
-	public GoodsView factory(Goods goods) {
+	public FoodView factory(Goods goods) {
 		Food food = (Food) goods;
 		return new FoodView(food.getName(), food.getPrice().toString(), food.getExpirationDate().toString());
 	}
